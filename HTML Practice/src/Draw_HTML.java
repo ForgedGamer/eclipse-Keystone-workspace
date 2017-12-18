9647f5bce437e9e5f82f8621ae87df7fcaf6e9f5
ForgedGamer <ForgedGamer@github.com>
2017-12-18 15:15:01 -0800

    HTML Practice added to file

---
+++ HTML Practice/src/Draw_HTML.java
@@ -0,0 +1,10 @@
+
+public class Draw_HTML extends java.applet.Applet 
+{
+	public void paint(java.awt.Graphics g)
+	{
+		g.drawString("Hello World!", 50, 25);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ HTML Practice/src/practiceAct/Names.java
@@ -0,0 +1,13 @@
+package practiceAct;
+
+public class Names extends java.applet.Applet
+{
+	public void paint(java.awt.Graphics g)
+	{
+		g.drawString("Petter", 50, 15);
+		g.drawString("William", 50, 30);
+		g.drawString("Harry", 50, 45);
+		g.drawString("Judes", 50, 65);
+	}
+
+}
